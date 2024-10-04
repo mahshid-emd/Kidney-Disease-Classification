ba2abeac293da6ca8abe87acb57b78fd2bd14bb5
sky <mahshid96e@yahoo.com>
2024-10-04 18:45:53 +0330

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-
-__version__ = "0.0.0"
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-SRC_REPO = "cnnClassifier"
+__version__ = '0.0'
+SRC_REPO = 'cnnClassifier'
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    description="A small python package for CNN app",
-    long_description_content="text/markdown",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    description='A small python package for CNN app',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src')
 )
--- template.py
+++ template.py
@@ -1,9 +1,9 @@
 import os
-from pathlib import Path
-import logging
+from pathlib import Path 
+import logging 
 
-#logging string
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+#logging string 
+logging.basicCinfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = 'cnnClassifier'
 
@@ -31,16 +31,14 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-
-    if filedir !="":
+    if filedir != '':
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+        logging.info(f'Creating directory: {filedir} for the file: {filename}')
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
             pass
-            logging.info(f"Creating empty file: {filepath}")
-
+            logging.info(f'Creating empty file: {filepath}')
 
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f'{filename} is already exists')
